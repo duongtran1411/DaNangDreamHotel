3744b6b91b0e933dfb2042a13f873aee7945361d
Tuấn Minh <136489572+minh11122@users.noreply.github.com>
2024-05-27 15:10:38 +0700

    update

--- src/java/Entity/RegistrationDTO.java
+++ src/java/Entity/RegistrationDTO.java
@@ -31,6 +31,70 @@ public class RegistrationDTO {
         this.phone = phone;
         this.role_Id = role_Id;
     }
+
+    public int getAccount_Id() {
+        return account_Id;
+    }
+
+    public void setAccount_Id(int account_Id) {
+        this.account_Id = account_Id;
+    }
+
+    public int getJob_Id() {
+        return job_Id;
+    }
+
+    public void setJob_Id(int job_Id) {
+        this.job_Id = job_Id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public int getRole_Id() {
+        return role_Id;
+    }
+
+    public void setRole_Id(int role_Id) {
+        this.role_Id = role_Id;
+    }
     
     
 }
